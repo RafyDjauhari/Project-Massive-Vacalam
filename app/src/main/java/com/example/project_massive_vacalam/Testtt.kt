029058e66fe2c671400e5ad605d60db3e7e4d235
MARS <sdamar321@gmail.com>
2023-06-08 23:48:48 +0700

    test

--- app/src/main/java/com/example/project_massive_vacalam/Testtt.kt
+++ app/src/main/java/com/example/project_massive_vacalam/Testtt.kt
@@ -3,9 +3,9 @@ package com.example.project_massive_vacalam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Test : AppCompatActivity() {
+class Testtt : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        setContentView(R.layout.activity_testtt)
     }
 }
