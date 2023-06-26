80a10dbdc5c4cddbdafbf7c486e7e545cfccd187
RafyDjauhari <rafydjauhari15@gmail.com>
2023-06-26 11:27:32 -0700

    Update UPDATE SUPER DUPER BANYAK BANGET!!!

--- app/src/main/java/com/example/project_massive_vacalam/Bantuan.kt
+++ app/src/main/java/com/example/project_massive_vacalam/Bantuan.kt
@@ -12,48 +12,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Bantuan : AppCompatActivity() {
 
-    private lateinit var backBantuan: ImageButton
-    private lateinit var bantuanKuliner: FrameLayout
-    private lateinit var bantuanCariWisata: FrameLayout
-    private lateinit var pesanTiket: FrameLayout
-    private lateinit var pembayaran: FrameLayout
-    private lateinit var ubahKataSandi: TextView
-    private lateinit var Logout: TextView
+    private lateinit var backBantuan: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bantuan)
 
-        bantuanKuliner = findViewById<FrameLayout>(R.id.bantuan_kuliner)
-        bantuanCariWisata = findViewById(R.id.bantuan_cari_wisata)
-        pesanTiket = findViewById(R.id.bantuan_pesan_tiket)
-        pembayaran = findViewById(R.id.bantuan_pembayaran)
-        ubahKataSandi = findViewById(R.id.bantuan_kata_sandi)
-        Logout = findViewById(R.id.bantuan_logout)
-
-        val backBantuan = findViewById<ImageButton>(R.id.btn_back_bantuan)
+        backBantuan = findViewById(R.id.btn_back_bantuan)
         backBantuan.setOnClickListener {
             onBackPressed()
         }
 
-        bantuanKuliner.setOnClickListener {
-            val intent = Intent(this, Jelajahi::class.java)
-            startActivity(intent)
-        }
-
-        bantuanCariWisata.setOnClickListener {
-            val intent = Intent(this, Search::class.java)
-            startActivity(intent)
-        }
-
-        ubahKataSandi.setOnClickListener {
-            val intent = Intent(this, EditProfil::class.java)
-            startActivity(intent)
-        }
-
-        Logout.setOnClickListener {
-            val intent = Intent(this, Profil::class.java)
-            startActivity(intent)
-        }
     }
 }
--- app/src/main/java/com/example/project_massive_vacalam/PesanTiket.kt
+++ app/src/main/java/com/example/project_massive_vacalam/PesanTiket.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 
 class PesanTiket : AppCompatActivity() {
+
     private var jumlahAnak = 0
     private var jumlahDewasa = 0
 
@@ -20,6 +25,19 @@ class PesanTiket : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pesan_tiket)
+
+        val hari = listOf("Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu")
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete_txt)
+        val adapter = ArrayAdapter(this, R.layout.list_hari_kunjungan, hari)
+
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
+                adapterView, view, i, l ->
+
+            val hariSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this, "Item: $hariSelected", Toast.LENGTH_SHORT).show()
+        }
 
         val backButton = findViewById<ImageView>(R.id.backButton)
         val btnPembayaran = findViewById<LinearLayout>(R.id.btnPembayaran)
